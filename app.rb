ca6fef5f765c37ad2f21b910df29597dd8df7d69
Marco Martinez <marco.antoni.martinez@gmail.com>
2025-08-16 23:12:29 +0000

    added input and labels for homepage

--- app.rb
+++ app.rb
@@ -2,8 +2,12 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+get("/process_roll") do
+
+  
+  erb(:roll)
+
 end
