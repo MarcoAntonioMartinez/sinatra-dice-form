95d74441fa39cefa2413204fd966bb54079b7185
Marco Martinez <marco.antoni.martinez@gmail.com>
2025-08-17 00:00:38 +0000

    Added dice rolling functionality

--- app.rb
+++ app.rb
@@ -7,7 +7,16 @@ end
 
 get("/process_roll") do
 
+  @dice = params.fetch("dice").to_i
+  @sides = params.fetch("sides").to_i
+
+  @rolls = []
   
+  @dice.times do
+    die = rand(1..@sides)
+
+    @rolls.push(die)
+  end
   erb(:roll)
 
 end
